8b9b38d1b324e83cb29d57f1b85f500517f21c51
Gabriel Dominic Francia (PH) <G.Francia@homecredit.ph>
2026-08-05 18:26:07 +0800

    add helpers for *int and *string

--- helper.go
+++ helper.go
@@ -5,6 +5,16 @@ func BoolPtr(b bool) *bool {
 	return &b
 }
 
+// IntPtr retruns pointer to int
+func IntPtr(i int) *int {
+	return &i
+}
+
+// IntString returns pointer to string
+func IntString(s string) *string {
+	return &s
+}
+
 // TruePtr returns pointer to true
 func TruePtr() *bool {
 	return BoolPtr(true)
